9ad4696a2fb8e37474c90d3aff89f9845cf45421
Wormyy <167223943+BadCoder228@users.noreply.github.com>
2025-12-02 16:00:51 +0700

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -6,6 +6,7 @@ from colorama import Fore
 from colorama.ansi import AnsiFore
 
 VERSION = 1.1
+PATH = f"C:/Users/{os.getlogin()}/AppData/LocalLow/Landfall Games/Content Warning/Saves"
 
 #functions
 def main() -> None:
@@ -30,10 +31,27 @@ def timer(seconds : int = 5) -> None:
 def inject() -> None:
     try:
         # Editing the data file
-        with open(f"C:/Users/{os.getlogin()}/AppData/LocalLow/Landfall Games/Content Warning/Saves/metaData.m", "w") as file:
+        with open(PATH + "/metaData.m", "w") as file:
             file.write("""SerializedMetaProgression, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null|{"MetaCoins":1000000,"UnlockedHats":[0,1,2,3,4,5,6,7,9,10,11,12,13,15,16,17,18,19,20,14,30,8,21,22,23,24,25,26,27,28,29],"UnlockedIslandUpgrades":[0,4,2,3,1]}""")
             file.close()
-        
+
+        for i in os.listdir(PATH):
+            current_path = f"{PATH}/{i}"
+            if os.path.isfile(current_path) and "Save" in i:
+                
+                with open(current_path, 'r') as file:
+                    file_data = file.read()
+                    file.close()
+
+                file_dict = dict(file_data[96:])
+                file_data = file_data[:97]
+
+                file_dict["Money"] = 1000000
+
+                with open(current_path, "w") as file:
+                    file.write(file_data+file_dict)
+                    file.close()
+
         # Success!
         banner(Fore.GREEN, "Success!", "The game data has been overwritten successfully!")
 
