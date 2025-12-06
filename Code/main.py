4e884dd94aefc3d680f7e0c005182ef9a13a3259
Wormyy <167223943+BadCoder228@users.noreply.github.com>
2025-12-07 00:06:50 +0700

    V. 1.4.0
    An update. A final one i'd even say

--- Code/main.py
+++ Code/main.py
@@ -1,6 +1,7 @@
+""" LIBRARIES """
+
 from sys import exit
 from art import tprint
-from time import sleep
 from colorama import Fore
 from json import dump, load
 from ast import literal_eval
@@ -8,10 +9,12 @@ from os.path import isfile, exists
 from colorama.ansi import AnsiFore
 from os import getlogin, listdir, system
 
+""" VARIABLES """
+
 # const data
 VERSION_MAJOR: str = 1
-VERSION_MINOR: str = 2
-VERSION_PATCH: str = 4
+VERSION_MINOR: str = 4
+VERSION_PATCH: str = 0
 PATH: str = f"C:/Users/{getlogin()}/AppData/LocalLow/Landfall Games/Content Warning/Saves"
 
 # default data
@@ -20,80 +23,90 @@ MC_MONEY: int = 1000000
 ISLAND_UPGRADES: list = list(range(0, 5))
 UNLOCKED_HATS: list = list(range(0, 31))
 
-#functions
+""" FUNCTIONS """
+
+# main 
 def main() -> None:
     if not exists("data.json"):
         create_file()
 
-    banner(Fore.MAGENTA, "The  Cracker Program", f"by Wormyy\nv. {VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}\n")
-    while asker() != 0:
-        asker()
+    banner(Fore.MAGENTA, "The  Cracker Program", f"by Wormyy\nV. {VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}\n")
+    asker()
     
-def asker() -> int:
-    action: str = input("1. Start injection\n2. Overwrite values\n3. Go to default values\n\n>>>")
+# asks for a number, then calls a function
+def asker() -> None:
+    action: str = input("1. Start injection\n2. Overwrite values\n3. Go to default values\n4. Leave\n\n>>> ")
     match(action):
         case "1":
             inject()
         case "2":
             overwrite()
         case "3":
-            create_file()
-            banner(Fore.GREEN, "Done!", "The values have been overwritten to default ones!\n")
-            timer(shutdown=False)
-            main()
+            go_to_default()
+        case "4":
+            leave(True)
         case _:
             print(Fore.RED + "\nUnknown option\n\n" + Fore.WHITE)
-            return 1
-    return 0
+            asker()
 
+# cre8s cul banner
 def banner(color : AnsiFore = Fore.WHITE, title: str = "", subtitle: str = "") -> None:
     system("cls")
     print(color)
     tprint(title)
     print(subtitle + Fore.WHITE)
 
-def timer(seconds : int = 5, shutdown: bool = True) -> None:
-    for i in range(seconds):
-        print(f"{'leaving' if shutdown else 'going back to the main menu'} in {seconds - i - 1} seconds.", end="\r" if i + 1 != seconds else "\n")
-        sleep(1)
-    else:
-        if (shutdown):
-            print("Closing the app...")
-            exit() # and i know that i can use the 'quit' key phrase but it gives an error at the last second
-
+# overwrites injecting values
 def overwrite() -> None:
     new_data: dict = {"World_money":None,"Mc_money":None,"Island_buildings":None,"Hats":None}
     for i in range(4):
         key_var: str = list(new_data.keys())[i]
-        banner(Fore.LIGHTYELLOW_EX, "Rewrite the value", f"Set a value for a \"{key_var}\" variable\n")
+        banner(Fore.LIGHTYELLOW_EX, "Rewrite  the value", f"Set a value for a \"{key_var}\" variable\nif you don't want to change the value of this variable press \"enter\"")
         
-        new_value: str = input(">>>")
+        new_value: str = input(">>> ")
         
         while True:
-            try:
-                if "-" not in new_value and int(new_value):
-                    break
-                else:
-                    raise()
-            except:
-                print(Fore.RED + "Error! Wrong value type" + Fore.WHITE)
-                new_value: str = input(">>>")
+            if new_value.isdigit() and int(new_value) >= 0 or new_value == "":
+                break
+            else:
+                print(Fore.RED + "Error! Cause: wrong value type" + Fore.WHITE)
+                new_value: str = input(">>> ")
         
-        new_data[key_var] = int(new_value) if i in [0, 1] else list(range(0, int(new_value) if int(new_value) <= 100 else 200))
+        if new_value == "":
+            new_data[key_var] = gain_data()[key_var]
+            continue
+        else:
+            new_data[key_var] = int(new_value) if i in [0, 1] else list(range(0, int(new_value) if int(new_value) <= 100 else 200))
     else:
         create_file(new_data)
         banner(Fore.GREEN, "Done!", "The values have been overwritten!\n")
-        timer(shutdown=False)
-        main()
+        leave()
 
+# brings default values back
+def go_to_default():
+    create_file()
+    banner(Fore.GREEN, "Done!", "The values have been overwritten to default ones!\n")
+    leave()
+
+# cre8s/edits a file
 def create_file(dictionary_to_write : dict = {"World_money": MONEY,"Mc_money":MC_MONEY,"Island_buildings":ISLAND_UPGRADES,"Hats":UNLOCKED_HATS}) -> None:
     with open("data.json", "w") as file:
         dump(dictionary_to_write, file)
 
+# user action check
+def leave(quit: bool = False) -> None:
+    if quit:
+        exit()
+    else:    
+        input("press \"enter\"\n>>> ")
+        main()
+
+# reads the file with data
 def gain_data() -> dict:
     with open("data.json") as file:
         return load(file)
 
+# injects
 def inject() -> None:
     try: 
         gained_data: dict = gain_data()
@@ -135,8 +148,8 @@ def inject() -> None:
 
         finally:
             print("\n\n")
-            timer()
+            leave()
 
-# Launch the program
+# Launches the program
 if __name__ == '__main__':
     main()
